e84310e7515004ad59332332b2d9849a5d57b581
Guilherme R Woelke <guilherme_wolke@yahoo.com>
2023-10-06 16:05:19 -0300

    main
    - remoção da pasta client

--- client_app/src/Components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-    return (
-        <footer> 
-            <small>Este sistema foi desenvolvido utilizando React.js e Java SpringBoot 3.0</small>
-        </footer>
-    );
-}
